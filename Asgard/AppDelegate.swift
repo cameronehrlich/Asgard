daa7517079cc051dae085ae9a4bb4e9afdda3619
cameronehrlich <cameronehrlich@gmail.com>
2016-01-13 21:37:09 -0800

    Initial Commit

---
+++ Asgard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Asgard
+//
+//  Created by Cameron Ehrlich on 1/13/16.
+//  Copyright © 2016 Cameron Ehrlich. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Asgard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Asgard
+//
+//  Created by Cameron Ehrlich on 1/13/16.
+//  Copyright © 2016 Cameron Ehrlich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
